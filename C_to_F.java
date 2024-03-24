35e53fd75ec6e09802cc0849b2cb405dd7d028ad
se9zr <157512511+se9zr@users.noreply.github.com>
2024-03-24 05:47:49 +0300

    Create C_to_F.java
    Lab 1 task 1

---
+++ C_to_F.java
@@ -0,0 +1,12 @@
+
+public class C_to_F
+{
+    public static void main(String [] args)
+    {
+        double C = 40;
+        double F;
+        
+        F = (9./5*C+32);
+        System.out.println("Celsius Degree "+C+" = "+F+" in Fahrenheit!");
+    }
+}
