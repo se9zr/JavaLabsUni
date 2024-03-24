1a741fdc70ba9a2b65de9ee9021b6f592236bbba
se9zr <157512511+se9zr@users.noreply.github.com>
2024-03-24 05:49:56 +0300

    Lab 1 task 2
    GradesAverage

---
+++ GradesAverage.java
@@ -0,0 +1,13 @@
+public class GradesAverage 
+{
+    public static void main(String[] args )
+    {   
+        //Using double instead of int to get decemli vaules
+        double grade1=82 , grade2=95, grade3=74;
+        double average;
+        
+        //Logic Erorr(solved by parthinses)
+        average = (grade1 + grade2 + grade3) / 3;
+        System.out.println("Average of greades = "+average);
+    }
+}
